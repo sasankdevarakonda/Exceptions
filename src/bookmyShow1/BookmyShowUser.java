207f53c22c3bed93489bd3329a9bb1cb5cb37ca6
sasankdevarakonda <149362760+sasankdevarakonda@users.noreply.github.com>
2024-03-06 12:40:29 +0530

    WhileLoopTest

--- src/bookmyShow1/BookmyShowUser.java
+++ src/bookmyShow1/BookmyShowUser.java
@@ -4,7 +4,7 @@ public class BookmyShowUser {
     public static void main(String[] args) {
         try {
             TicketBookingProcess process = new TicketBookingProcess(7);
-            process.bookingseats("Sasank", "Sasi@12345");
+            process.bookingseats("Sasank", "Sasi@1234");
         } catch (PaymentException paymentException) {
             System.out.println(paymentException.getPayment()+" "+paymentException.getMessage());
         }
---
+++ src/fileNotFound/WhileLoopTest.java
@@ -0,0 +1,40 @@
+package fileNotFound;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class WhileLoopTest {
+    public static void main(String[] args) {
+        
+                while (true) {
+                    try {
+                        FileInputStream fis = new FileInputStream("D:\\WhileTest\\sasi");
+                        processFile();
+                        fis.close();
+                        break;
+                    } catch (FileNotFoundException e) {
+                        System.out.println("File not found, retrying in 1 minute...");
+                        System.out.println(e.getMessage());
+                        try {
+                            Thread.sleep(1 * 60 * 1000);
+                        } catch (InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        break;
+                    }
+                }
+            }
+
+            private static void processFile() throws FileNotFoundException {
+                File file = new File("D:\\WhileTest\\sasi");
+                if (!file.exists()) {
+                    throw new FileNotFoundException();
+                }
+                System.out.println("File found! Processing...");
+            }
+        }
+
